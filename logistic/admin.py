92164f8b09a8df6c6456e6fd0f91ea33a8fa4f0f
volik97 <volik97@inbox.ru>
2022-07-22 21:11:47 +0300

    first commit

---
+++ logistic/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from logistic.models import Product, Stock,StockProduct
+
